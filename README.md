72fc2496f24b470cbb803b56bb2c8353b10fa15c
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-10 12:01:16 -0500

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# sync-shippo-to-zendesk
+# @particular./sync-shippo-to-zendesk
+
+[![npm version](https://badge.fury.io/js/%40particular.%2Fsync-shippo-to-moltin.svg)](https://badge.fury.io/js/%40particular.%2Fsync-shippo-to-moltin)
+
+> Add a Zendesk Sunshine Event whenever a Shipping Status change is triggered in Shippo
+
+Asynchronous microservice that is triggered by [Shippo](https://goshippo.com) webhooks to create a Sunshine Event inside of [Zendesk](https://zendesk.com).
+
+Built with [Micro](https://github.com/zeit/micro)! 🤩
+
+## 🛠 Setup
+
+Both a [Zendesk](https://zendesk.com) _and_ [Shippo](https://goshippo.com) account are needed for this to function.
+
+Create a `.env` at the project root with the following credentials:
+
+```dosini
+ZENDESK_SUBDOMAIN=
+ZENDESK_INTEGRATION_EMAIL=
+ZENDESK_INTEGRATION_SECRET=
+```
+
+`ZENDESK_SUBDOMAIN` is the first part of the URL for your Zendesk account (ie. https://{ZENDESK_SUBDOMAIN}.zendesk.com/).
+
+While logged in to your Zendesk instance create a new User to run the Webhooks under by going to `Settings` > `People` > `Add User` > `Role: Staff`; this email address will be used as your `ZENDESK_INTEGRATION_EMAIL` above.
+
+Find your `ZENDESK_INTEGRATION_SECRET` within your Zendesk instance by going to `Settings` > `API` > enable `Token Access` > add `Active API Tokens [+]` > `API Token`.
+
+## 📦 Package
+
+Run the following command to build the app
+
+```bash
+yarn install
+```
+
+Start the development server
+
+```bash
+yarn dev
+```
+
+The server will typically start on PORT `3000`, if not, make a note for the next step.
+
+Start ngrok (change ngrok port below from 3000 if yarn dev deployed locally on different port above)
+
+```bash
+ngrok http 3000
+```
+
+Make a note of the https `ngrok URL` provided.
+
+## ⛽️ Usage
+
+Next head over to the Shippo [API Settings](https://app.goshippo.com/settings/api) area, add a new webhook with the following details:
+
+| Event Type    | Mode   | URL                 |
+| ------------- | ------ | ------------------- |
+| Track Updated | `Test` | _`ngrok URL` above_ |
+
+## 🚀 Deploy
+
+You can easily deploy this function to [now](https://now.sh).
+
+_Contact [Adam Grohs](https://www.linkedin.com/in/adamgrohs/) @ [Particular.](https://uniquelyparticular.com) for any questions._
---
+++ index.js
@@ -0,0 +1,191 @@
+const { json, send } = require('micro')
+const moment = require('moment-timezone')
+const fetch = require('node-fetch')
+
+const cors = require('micro-cors')({
+  allowMethods: ['POST']
+})
+
+const _toJSON = error => {
+  return !error
+    ? ''
+    : Object.getOwnPropertyNames(error).reduce(
+        (jsonError, key) => {
+          return { ...jsonError, [key]: error[key] }
+        },
+        { type: 'error' }
+      )
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(
+    'Promise unhandledRejection: ',
+    p,
+    ', reason:',
+    JSON.stringify(reason)
+  )
+})
+
+module.exports = cors(async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return send(res, 200, 'ok!')
+  }
+
+  /*
+  "carrier": "usps",
+  "tracking_number": "9205590164917312751089",
+  "address_from": {
+    "city": "Las Vegas",
+    "state": "NV",
+    "zip": "89101",
+    "country": "US"
+  },
+  "address_to": {
+    "city": "Spotsylvania",
+    "state": "VA",
+    "zip": "22551",
+    "country": "US"
+  },
+  "transaction": "1275c67d754f45bf9d6e4d7a3e205314",
+  "original_eta": "2016-07-23T00:00:00Z",
+  "eta": "2016-07-23T00:00:00Z",
+  "servicelevel": {
+    "token": "usps_priority",
+    "name": "Priority Mail"
+  },
+  "metadata": null,
+  "tracking_status": {
+    "object_created": "2016-07-23T20:35:26.129Z",
+    "object_updated": "2016-07-23T20:35:26.129Z",
+    "object_id": "ce48ff3d52a34e91b77aa98370182624",
+    "status": "DELIVERED",
+    "status_details": "Your shipment has been delivered at the destination mailbox.",
+    "status_date": "2016-07-23T13:03:00Z",
+    "location": {
+      "city": "Spotsylvania",
+      "state": "VA",
+      "zip": "22551",
+      "country": "US"
+    }
+  },
+  */
+
+  try {
+    const tracking_update = await json(req)
+    const {
+      data: {
+        carrier,
+        tracking_number,
+        servicelevel: { name: service_type },
+        eta,
+        tracking_status: {
+          status,
+          status_details,
+          status_date,
+          location: {
+            city: location_city,
+            state: location_state,
+            zip: location_zip
+          }
+        }
+      }
+    } = tracking_update
+
+    let tracking_extra = {}
+    if (tracking_update.extra) {
+      tracking_extra = tracking_update.extra
+    }
+    let { billing_email, order_id } = tracking_extra
+    if (!order_id) {
+      order_id = '730589c9-ee68-44b4-a201-bb38a9468abe'
+    }
+    if (!billing_email) {
+      billing_email = 'adam@uniquelyparticular.com'
+    }
+
+    if (order_id) {
+      if (billing_email) {
+        const payload = {
+          profile: {
+            source: 'support',
+            identifiers: {
+              email: billing_email
+            }
+          },
+          event: {
+            source: 'shippo',
+            type: 'shipping-status-change',
+            description:
+              status === 'DELIVERED' ? 'Order Delivered' : 'Shipping Update',
+            created_at: moment(status_date),
+            properties: {
+              'Shipping Carrier': carrier,
+              'Tracking Status': status, //TODO: format case
+              'Order ID': order_id,
+              'Tracking Number': `<a href="tracking_number">${tracking_number}</a>`,
+              'Service Type': service_type,
+              'Estimated Delivery': moment(eta).format('MM/DD/YYYY'),
+              'Tracking Notes': status_details,
+              'Tracking Location': `${location_city}, ${location_state} ${location_zip}`
+            }
+          }
+        }
+
+        fetch(
+          `https://${
+            process.env.ZENDESK_SUBDOMAIN
+          }.zendesk.com/api/sunshine/track`,
+          {
+            headers: {
+              Authorization: `Basic ${Buffer.from(
+                `${process.env.ZENDESK_INTEGRATION_EMAIL}/token:${
+                  process.env.ZENDESK_INTEGRATION_SECRET
+                }`
+              ).toString('base64')}`,
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(payload)
+          }
+        )
+          .then(response => {
+            if (response.ok && response.status < 299) {
+              return send(
+                res,
+                200,
+                JSON.stringify({ received: true, order_id })
+              )
+            } else {
+              return send(res, 500, 'Error')
+            }
+          })
+          .catch(error => {
+            const jsonError = _toJSON(error)
+            return send(
+              res,
+              jsonError.type === 'StripeSignatureVerificationError' ? 401 : 500,
+              jsonError
+            )
+          })
+      } else {
+        console.error('missing billing_email')
+        return send(res, 200, JSON.stringify({ received: true, order_id }))
+      }
+    } else {
+      console.error('missing order_id')
+      return send(
+        res,
+        200,
+        JSON.stringify({ received: true, order_id: 'null' })
+      )
+    }
+  } catch (error) {
+    const jsonError = _toJSON(error)
+    return send(
+      res,
+      jsonError.type === 'StripeSignatureVerificationError' ? 401 : 500,
+      jsonError
+    )
+  }
+})
